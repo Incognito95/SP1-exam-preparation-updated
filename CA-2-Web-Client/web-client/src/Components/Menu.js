0bb58b35d7ca0f1c7b5b661e4bd364f43266a23f
incognito95 <danielweisspedersen95@hotmail.com>
2022-01-12 15:07:36 +0100

    added logo to menu

--- CA-2-Web-Client/web-client/src/Components/Menu.js
+++ CA-2-Web-Client/web-client/src/Components/Menu.js
@@ -10,7 +10,7 @@ function Menu () {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <NavLink exact active className="navbar-brand" to="/"><img src="/images/logo.png" width="45" /></NavLink>
+            <NavLink exact active className="navbar-brand" to="/"><img src="logo.png" width="60" /></NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
